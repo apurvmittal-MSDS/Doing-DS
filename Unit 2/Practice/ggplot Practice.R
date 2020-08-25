972f7dbd95c504dff6b9396f42cf6431d665833d
apurvmittal-MSDS <64920515+apurvmittal-MSDS@users.noreply.github.com>
2020-08-24 22:59:54 -0500

    Unit 2 Practice of ggplot and ggplot2

--- Unit 2/Practice/ggplot Practice.R
+++ Unit 2/Practice/ggplot Practice.R
@@ -19,3 +19,39 @@ mpg%>%ggplot(mapping = aes(x=hwy, y=cty, linetype=drv, color = drv))+geom_smooth
 #Scatterplot with smooth line for different value of Class for both points and line and line by drive as well
 mpg%>%ggplot(mapping = aes(x=hwy, y=cty, linetype=drv, color = class))+geom_smooth()+geom_point()
 
+#DIFFERENT WAY - Scatterplot with smooth line for different value of Class for both points and line and line by drive as well
+mpg%>%ggplot()+
+geom_point(mapping = aes(x=hwy, y=cty, color = class)) +
+geom_smooth(mapping = aes(x=hwy, y=cty, linetype=drv, color=drv))
+
+#DIFFERENT WAY - FACET_Wrap
+mpg%>%ggplot()+
+  geom_point(mapping = aes(x=hwy, y=cty, color = class)) +
+  geom_smooth(mapping = aes(x=hwy, y=cty, linetype=drv, color=drv))+
+  facet_wrap(~class)
+
+#DIFFERENT WAY - FACET_grid
+mpg%>%ggplot()+
+  geom_point(mapping = aes(x=hwy, y=cty, color = class)) +
+  geom_smooth(mapping = aes(x=hwy, y=cty, linetype=drv, color=drv))+
+  facet_grid(drv~class)
+
+## Transformations
+
+
+
+
+## MAPS
+
+install.packages("maps")
+library(maps)
+?map_data
+
+##  Themes
+
+install.packages("ggthemes")
+library(ggthemes)
+
+mpg%>%ggplot()+
+  geom_point(mapping = aes(x=hwy, y=cty, color = class)) +
+  geom_smooth(mapping = aes(x=hwy, y=cty, linetype=drv, color=drv))+theme_wsj()
