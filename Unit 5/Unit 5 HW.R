a5a91aceba0a9066e20a193649f4d74b468fe5fc
apurvmittal-MSDS <64920515+apurvmittal-MSDS@users.noreply.github.com>
2020-09-20 11:29:24 -0500

    Unit 5 HW

--- Unit 5/Unit 5 HW.R
+++ Unit 5/Unit 5 HW.R
@@ -83,9 +83,11 @@ Babies<-inner_join(y.2015,fy2016, by="Name")
 #Filter out any NA values
 Final = Babies%>%filter(!is.na(Count.2015)| !is.na(Count.2016))
 
-# Converting the Geender to Factor
+# Converting the Gender to Factor
 Final$Gender.2015=as.factor(Final$Gender.2015)
 Final$Gender.2016=as.factor(Final$Gender.2016)
+
+# 3.3#
 
 # Creating a new Column with the total count of 2015 and 2016
 Final$Total = Final$Count.2015+Final$Count.2016
@@ -105,3 +107,43 @@ Girls<-Final%>%filter(Gender.2015 =="F"& Gender.2016 =="F")
 
 # write to csv
 write.csv(Girls[-c(2:5)], file = "/Users/apurv/Documents/SMU/6306 - Doing Data Science/Unit - 5/Unit5_Girl_Babies_Apurv.csv")
+
+########################################
+######### Question 4 ###################
+########################################
+########################################
+########################################
+
+
+# sorted 2015 Data
+y.2015 <- y.2015[order(y.2015$Count.2015, decreasing = TRUE),]
+
+TopTen2015<-head(y.2015,n=10)
+
+# sorted 2016 Data
+fy2016 <- fy2016[order(fy2016$Count.2016, decreasing = TRUE),]
+TopTen2016<-head(y.2016,n=10)
+
+TopTen = data.frame(TopTen2015,TopTen2016)
+
+Final%>%ggplot(aes(x=Gender.2015))+geom_bar()
+
+Final%>%ggplot(aes(x=Gender.2016))+geom_bar()
+
+
+########################################
+
+sum(str_count(Final$Name,"(\\bA\\b)"))
+
+str_count(Final$Name,"(\\. A| \"A)")
+
+matchDF %>% ggplot(aes(x = Colors, fill = Colors)) + geom_bar()+ scale_fill_manual(values=colors[order(colors)])
+
+# Calculating the percentage of name popularity
+Final$Percent = (Final$Total/sum(Final$Total))
+
+
+top08 <- Final %>% select(Name, Percent, Gender.2015) %>% arrange(desc(Percent)) %>% group_by(Gender.2015) %>% top_n(n =10, wt = Percent)
+
+
+ggplot(top08, aes(top08, x = reorder(Name, Percent),y = Percent, fill = Gender.2015)) + geom_bar(stat = "identity") + coord_flip() + xlab("") + ylab("") + ggtitle(label="Most popular baby names in 2015 and 2016") + scale_y_continuous(labels = scales::percent) + facet_wrap(c("Gender.2015"), scales = "free_y")  + theme_minimal() + theme(legend.position="none")  + scale_fill_hue(l=40, c=35) + scale_fill_manual(values=c("blue", "red"))
